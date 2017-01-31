689a34555998e8e7c871a92599ad73daf22685ad
wmattern0 <wm.m.mattern@gmail.com>
2017-01-31 14:18:48 -0500

    Cat Constructors tests built

--- src/test/java/io/zipcoder/pets/CatTest.java
+++ src/test/java/io/zipcoder/pets/CatTest.java
@@ -8,4 +8,19 @@ import static org.junit.Assert.*;
  * Created by williammattern on 1/31/17.
  */
 public class CatTest {
+    Cat smokey;
+
+    @Test
+    public void speakTest(){
+        smokey = new Cat("smokey");
+        String expected = "Meow", actual = smokey.speak();
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void nameTest(){
+        smokey = new Cat("smokey");
+        String expected = "smokey", actual = smokey.getName();
+        assertEquals(expected,actual);
+    }
 }
