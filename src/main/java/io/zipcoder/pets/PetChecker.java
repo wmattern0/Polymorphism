aac6ce6652e7c367c84497af97b107a95ed51edd
wmattern0 <wm.m.mattern@gmail.com>
2017-02-01 15:47:02 -0500

    Passing Comparator Unit Tests

--- src/main/java/io/zipcoder/pets/PetChecker.java
+++ src/main/java/io/zipcoder/pets/PetChecker.java
@@ -1,8 +1,14 @@
 package io.zipcoder.pets;
 
+import java.util.Comparator;
+
 /**
  * Created by williammattern on 2/1/17.
  */
-public class PetChecker {
+public class PetChecker implements Comparator<Pet>{
 
+    @Override
+    public int compare(Pet pet1, Pet pet2) {
+        return pet1.compareTo(pet2);
+    }
 }
--- src/test/java/io/zipcoder/pets/PetCheckerTest.java
+++ src/test/java/io/zipcoder/pets/PetCheckerTest.java
@@ -8,10 +8,31 @@ import static org.junit.Assert.*;
  * Created by williammattern on 2/1/17.
  */
 public class PetCheckerTest {
+    PetChecker petChecker = new PetChecker();
+    Pet turbo;
+    Pet turbo2;
 
     @Test
     public void compareTestEquivalent(){
-
+        turbo = new Pet("Turbo");
+        turbo2 = new Pet("Turbo");
+        int actual = petChecker.compare(turbo,turbo2), expected = 0;
+        assertEquals(expected,actual);
     }
 
+    @Test
+    public void compareTest1Before2(){
+        turbo = new Pet("Turbo");
+        turbo2 = new Pet("Turbs");
+        int actual = petChecker.compare(turbo,turbo2), expected = -1;
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void compareTest1After2(){
+        turbo = new Pet("Turbs");
+        turbo2 = new Pet("Turbo");
+        int actual = petChecker.compare(turbo,turbo2), expected = 1;
+        assertEquals(expected,actual);
+    }
 }
